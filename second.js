c393d79c15b16d88fc2e1635d71e074fa22a8cc0
NamDo8467 <77046082+NamDo8467@users.noreply.github.com>
2021-02-10 21:11:30 +0700

    Update second.js

--- second.js
+++ second.js
@@ -1,7 +1,5 @@
-let audio = new Audio();
-audio = document.querySelector(".audio");
-let button = document.querySelector(".btn");
-button.addEventListener('click', () => {
+window.addEventListener("DOMContentLoaded", event => {
+  const audio = document.querySelector(".audio");
   audio.volume = 0.5;
   audio.play();
-})
+});
