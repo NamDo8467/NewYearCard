1799387185f20c59d5af0036180475777ef78a77
NamDo8467 <77046082+NamDo8467@users.noreply.github.com>
2021-02-10 22:28:54 +0700

    Update second.js

--- second.js
+++ second.js
@@ -1,5 +1,22 @@
-window.addEventListener("DOMContentLoaded", event => {
-  const audio = document.querySelector(".audio");
-  audio.volume = 0.5;
-  audio.play();
+let up = document.querySelector("#up");
+let muted = document.querySelector("#muted");
+let button2 = document.querySelector("#button2")
+const audio = document.querySelector(".audio");
+
+up.style.display = "inline";
+muted.style.display = "none";
+
+button2.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.5;
+    audio.play();
+    muted.style.display = "inline";
+    up.style.display = "none";
+  } else {
+    audio.pause();
+    up.style.display = "inline";
+    muted.style.display = "none";
+    
+  }
+
 });
